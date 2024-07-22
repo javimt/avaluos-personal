d6dbaf64c4a3b3d131c0e0a408773410f836370b
Nestor novella <importarnorth@gmail.com>
2024-07-22 18:22:41 -0300

    first commit

--- client/src/redux/store/store.js
+++
@@ -1,5 +0,0 @@
-import { applyMiddleware, legacy_createStore } from 'redux';
-import {thunk} from 'redux-thunk';
-import rootReducer from '../reducer/rootReducer';
-
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
