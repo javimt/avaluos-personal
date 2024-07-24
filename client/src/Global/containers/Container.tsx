fa5129360901a75d5764fa11b73b13acb578634a
nestornovellafullstackdev <nestornovellafullstackdev@gmail.com>
2024-07-24 19:04:16 +0000

    changes

--- client/src/Global/containers/Container.tsx
+++ client/src/Global/containers/Container.tsx
@@ -3,7 +3,7 @@
 export default function Container({ children }: { children: React.ReactNode }) {
 
     return (
-        <div className="mx-auto00 xl:px-52 md:px-20 px-2 xl:py-20 md:py-12 py-10">
+        <div className="relative mx-auto xl:px-52 md:px-20 px-2 xl:py-20 md:py-12 py-10">
             {children}
         </div>
     )
