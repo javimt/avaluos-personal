8a31e495f252aaeab377a8c64132c80d4484add7
nestornovellafullstackdev <nestornovellafullstackdev@gmail.com>
2024-08-02 16:08:40 +0000

    changes

--- client/src/03-about/About.tsx
+++ client/src/03-about/About.tsx
@@ -20,7 +20,6 @@ export default function About() {
                 </section>
                 {/* ---------- */}
                 <section >
-
                     <h1 className="text-primary text-4xl my-2 font-popins">Nacional de Avalùos</h1>
                     <h2 className="text-secondary text-2xl font-popins">Avaluos en toda Medellìn</h2>
                     <p className="text-gray-400 text-[16px] mt-2 xl:w-2/3 font-popins font-[300]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam impedit quisquam rerum voluptatum totam dignissimos corporis laudantium nostrum deleniti iste, odio officia omnis nobis cumque ut doloribus? Minus facere mollitia error laborum ab recusandae, illum repellat quia, consequatur provident delectus cupiditate, qui enim dolorem. Ab vero dicta quod ipsam ea reprehenderit exercitationem mollitia fuga veniam debitis, modi atque distinctio eveniet.</p>
