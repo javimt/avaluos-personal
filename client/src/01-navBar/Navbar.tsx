286f51564a860333cc6a78c384acd715504ee5e2
centerserviciotecnico <centerserviciotecnico@gmail.com>
2024-08-08 03:52:03 +0000

    react scroll

--- client/src/01-navBar/Navbar.tsx
+++ client/src/01-navBar/Navbar.tsx
@@ -1,68 +1,87 @@
-import logo1 from "../assets/logo1.jpeg"
+import logo1 from "../assets/definitivo.jpeg"
 import Li from "./Li"
 import { GiVibratingSmartphone } from "react-icons/gi";
 import Styles from './navbar.module.css'
 import { BsMenuButtonWideFill } from "react-icons/bs";
 import { useState } from "react";
 import useHandleScroll from "./hooks/useScroll";
+import { Link } from "react-scroll";
 
 export const NavBar = () => {
-    const [show, setShow] = useState(false)
-    const { scrolling } = useHandleScroll()
-    return (
-        <div className="mx-auto xl:px-52 md:px-20 px-2 xl:py-2 md:py-2 py-2  shadow-md relative">
-            <div className="flex justify-between items-center relative" >
-                <section className={`${Styles.logo} flex rounded-full overflow-hidden p-2 bg-primary opacity-0`}>
-                    <img className="size-[60px] " src={logo1} alt="" />
-                </section>
-                {
-                    !scrolling ?
-                        <section className={`md:flex hidden`}>
-                            <ul className="flex xl:gap-8 md:gap-5 gap-2">
-                                <Li>Home</Li>
-                                <Li>About</Li>
-                                <Li>Services</Li>
-                                <Li>Queries</Li>
-                                <Li>Contact</Li>
-                            </ul>
-                        </section>
-                        :
-                        <section className={`md:flex hidden fixed top-10 w-[500px] z-[1000] right-[calc(50%-250px)] p-2 backdrop-blur-3xl   justify-center rounded-2xl` }>
-                            <ul className="flex xl:gap-8 md:gap-5 gap-2 text-white">
-                                <Li color="white">Home</Li>
-                                <Li color="white">About</Li>
-                                <Li color="white">Services</Li>
-                                <Li color="white">Queries</Li>
-                                <Li color="white">Contact</Li>
-                            </ul>
-                        </section>
-                }
-                {
-                    show &&
-                    <section className={`block  md:hidden absolute z-[1000] bg-white top-10 w-svw -translate-x-2 translate-y-10 `}>
-                        <ul className="flex flex-col items-center xl:gap-8 md:gap-5 gap-2  ">
-                            <li className="text-primary font-popins font-semibold hover:bg-secondary hover:text-white w-full text-center p-2">Home</li>
-                            <li className="text-primary font-popins font-semibold hover:bg-secondary hover:text-white w-full text-center p-2">About</li>
-                            <li className="text-primary font-popins font-semibold hover:bg-secondary hover:text-white w-full text-center p-2">Services</li>
-                            <li className="text-primary font-popins font-semibold hover:bg-secondary hover:text-white w-full text-center p-2">Queries</li>
-                            <li className="text-primary font-popins font-semibold hover:bg-secondary hover:text-white w-full text-center p-2">Contact</li>
-                        </ul>
-                    </section>
-                }
-                <section className=" gap-2 justify-center items-center hidden md:flex ">
-                    <div className="">
-                        <GiVibratingSmartphone className="size-[30px] text-secondary animate-pulse" />
-                    </div>
-                    <div className="text-primary font-popins">
-                        +54 11 2542 0570
-                    </div>
-                </section>
-                <section className="md:hidden" onClick={() => setShow(!show)}>
-                    <BsMenuButtonWideFill className="text-[35px] mr-5 text-primary cursor-pointer hover:text-secondary" />
-                </section>
+  const [show, setShow] = useState(false)
+  const { scrolling } = useHandleScroll()
+  return (
+    <div className="mx-auto xl:px-52 md:px-20 px-2 xl:py-2 md:py-2 py-2  shadow-md relative">
+      <div className="flex justify-between items-center relative" >
+        <img className="w-[250px]" src={logo1} alt="" />
+        {
+          !scrolling ?
+            <section className={`md:flex hidden`}>
+              <ul className="flex xl:gap-8 md:gap-5 gap-2 text-primary ">
+                <li>
+                  <Link activeClass="active" to="inicio" spy={true} smooth={true} offset={-70} duration={500}>Inicio</Link>
+                </li>
+                <li>
+                  <Link activeClass="active" to="acercaDe" spy={true} smooth={true} offset={-70} duration={500}>Acerca de</Link>
+                </li>
+                <li>
+                  <Link activeClass="active" to="servicios" spy={true} smooth={true} offset={-70} duration={500}>Services</Link>
+                </li>
+                <li>
+                  <Link activeClass="active" to="preguntas" spy={true} smooth={true} offset={-70} duration={500}>Preguntas</Link>
+                </li>
+                <li>
+                  <Link activeClass="active" to="contacto" spy={true} smooth={true} offset={-70} duration={500}>Contacto</Link>
+                </li>
+              </ul>
+            </section>
+            :
+            <section className={`md:flex hidden fixed top-10 w-[500px] z-[1000] right-[calc(50%-250px)] p-2 backdrop-blur-3xl justify-center rounded-2xl`}>
+              <ul className="flex xl:gap-8 md:gap-5 gap-2 text-white">
+                <Li color="white"><Link activeClass="active" to="inicio" spy={true} smooth={true} offset={-70} duration={500}>Inicio</Link></Li>
+                <Li color="white"><Link activeClass="active" to="acercaDe" spy={true} smooth={true} offset={-70} duration={500}>Acerca de</Link></Li>
+                <Li color="white"><Link activeClass="active" to="servicios" spy={true} smooth={true} offset={-70} duration={500}>Services</Link></Li>
+                <Li color="white"><Link activeClass="active" to="preguntas" spy={true} smooth={true} offset={-70} duration={500}>Preguntas</Link></Li>
+                <Li color="white"><Link activeClass="active" to="contacto" spy={true} smooth={true} offset={-70} duration={500}>Contacto</Link></Li>
+              </ul>
+            </section>
+        }
+        {
+          show &&
+          <section className={`block  md:hidden absolute z-[1000] bg-white top-10 w-svw -translate-x-2 translate-y-10 `}>
+            <ul className="flex flex-col items-center xl:gap-8 md:gap-5 gap-2  ">
+              <li className="text-primary font-popins font-semibold hover:bg-secondary hover:text-white w-full text-center p-2">
+                <Link activeClass="active" to="inicio" spy={true} smooth={true} offset={-70} duration={500}>Inicio</Link>
+              </li>
+              <li className="text-primary font-popins font-semibold hover:bg-secondary hover:text-white w-full text-center p-2">
+                <Link activeClass="active" to="acercaDe" spy={true} smooth={true} offset={-70} duration={500}>Acerca de</Link>
+              </li>
+              <li className="text-primary font-popins font-semibold hover:bg-secondary hover:text-white w-full text-center p-2">
+                <Link activeClass="active" to="servicios" spy={true} smooth={true} offset={-70} duration={500}>Services</Link>
+              </li>
+              <li className="text-primary font-popins font-semibold hover:bg-secondary hover:text-white w-full text-center p-2">
+                <Link activeClass="active" to="preguntas" spy={true} smooth={true} offset={-70} duration={500}>Preguntas</Link>
+              </li>
+              <li className="text-primary font-popins font-semibold hover:bg-secondary hover:text-white w-full text-center p-2">
+                <Link activeClass="active" to="contacto" spy={true} smooth={true} offset={-70} duration={500}>Contacto</Link>
+              </li>
+            </ul>
+          </section>
+        }
+        <section className=" gap-2 justify-center items-center hidden md:flex ">
+          <div className="">
+            <GiVibratingSmartphone className="size-[30px] text-secondary animate-pulse" />
+          </div>
+          <div className="text-primary font-popins">
+            +54 11 2542 0570
+          </div>
+        </section>
+        <section className="md:hidden" onClick={() => setShow(!show)}>
+          <BsMenuButtonWideFill className="text-[35px] mr-5 text-primary cursor-pointer hover:text-secondary" />
+        </section>
 
-            </div>
+      </div>
 
-        </div>
-    )
+    </div>
+  )
 }
